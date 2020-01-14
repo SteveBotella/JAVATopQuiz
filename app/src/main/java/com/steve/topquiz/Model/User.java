fca232fd77b85ddf2f5282a67ef42206f73c724a
Zoruk <zorukgaming@gmail.com>
2020-01-14 20:20:17 +0100

    - First commit

---
+++ app/src/main/java/com/steve/topquiz/Model/User.java
@@ -0,0 +1,20 @@
+package com.steve.topquiz.Model;
+
+public class User {
+    private String mFirstname;
+
+    public String getmFirstname() {
+        return mFirstname;
+    }
+
+    public void setmFirstname(String mFirstname) {
+        this.mFirstname = mFirstname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mFirstname='" + mFirstname + '\'' +
+                '}';
+    }
+}
